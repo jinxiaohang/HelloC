724d0695d1904cc226f28ace37ab711d4f7cd91e
jinzhihang <191041707@qq.com>
2017-02-16 21:34:26 +0800

    https://github.com/jinxiaohang/HelloC.git第一次尝试

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("https://github.com/jinxiaohang/HelloC.git第1次尝试");
+    }
+}
